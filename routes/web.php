a800144dc42a1086453745efbfc770d9b08ab748
Mustapha Bouddahr <mustaphabouddahr.dev@gmail.com>
2025-07-17 19:18:52 +0100

    fix the css, set a redirect from / to /certificate

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CertificateController;
 
+// Redirect '/' to '/certificate'
+Route::redirect('/', '/certificate');
+
 Route::get('/certificate', [CertificateController::class, 'form'])->name('certificate.form');
 Route::post('/certificate', [CertificateController::class, 'generate'])->name('certificate.generate');
